1d9d5d16d687e770adcf12637e07b61ad2d2a81f
Ly Tran Hoang Hieu <lthhieu12.14lvl@gmail.com>
2024-08-25 18:36:35 +0700

    style in react-native

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.hello1}>Hello world 1!</Text>
+      <Text>Hello world 2!</Text>
     </View>
   );
 }
@@ -17,4 +16,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  hello1: {
+    color: 'red', fontSize: 30,
+    borderColor: 'green',
+    borderWidth: 1,
+    padding: 10,
+    fontWeight: '600'
+  }
 });
