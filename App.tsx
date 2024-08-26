2a3b605980c56dad42f1d7a1efa8e5b2759b9d89
Ly Tran Hoang Hieu <lthhieu12.14lvl@gmail.com>
2024-08-26 21:53:49 +0700

    render array

--- App.tsx
+++ App.tsx
@@ -1,38 +1,39 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
+
+interface IStudents {
+  id: number, name: string, age: number
+}
 
 export default function App() {
-  const [name, setName] = useState<string>('Hiếu')
-  const [number, setNumber] = useState<number>(0)
+  const [students, setStudents] = useState<IStudents[]>([
+    { id: 1, name: 'Hieu1', age: 18 },
+    { id: 2, name: 'Hieu2', age: 18 },
+    { id: 3, name: 'Hieu3', age: 18 },
+    { id: 4, name: 'Hieu4', age: 18 },
+    { id: 5, name: 'Hieu5', age: 18 },
+    { id: 6, name: 'Hieu6', age: 18 },
+    { id: 7, name: 'Hieu7', age: 18 },
+    { id: 8, name: 'Hieu8', age: 18 },
+    { id: 9, name: 'Hieu9', age: 18 },
+    { id: 10, name: 'Hieu10', age: 18 },
+    { id: 11, name: 'Hieu11', age: 18 },
+    { id: 12, name: 'Hieu12', age: 18 },
+    { id: 13, name: 'Hieu13', age: 18 },
+    { id: 14, name: 'Hieu14', age: 18 },
+    { id: 15, name: 'Hieu15', age: 18 },
+    { id: 16, name: 'Hieu16', age: 18 },
+  ])
   return (
     <View style={styles.container}>
-      <TextInput style={{
-        borderColor: 'green', borderWidth: 1, width: 200, padding: 15
-      }} onChangeText={(text) => setNumber(+text)} multiline keyboardType='numeric' maxLength={2} />
-      <Text style={styles.text}>Hello {name}!</Text>
-      <Text>My number is {number}</Text>
-      <View style={styles.buttonGroup}>
-        <Button
-          onPress={() => {
-            setNumber((old) => old + 1)
-          }}
-          title='Plus 1'
-        />
-        <Button
-          onPress={() => {
-            setNumber((old) => old - 1)
-          }}
-          title='Minus 1'
-        />
-        <Button
-          onPress={() => {
-            // setNumber(0)
-            alert('Hi')
-          }}
-          title='Reset'
-          color={'violet'}
-        />
-      </View>
+      <Text style={styles.title}>Hello world</Text>
+      <ScrollView>
+        {students.map((item) => {
+          return (<View style={styles.view} key={item.id}>
+            <Text>{item.name}</Text>
+          </View>)
+        })}
+      </ScrollView>
     </View>
   );
 }
@@ -41,22 +42,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'flex-start',
+    // justifyContent: 'flex-start',
+    paddingTop: 40,
+    paddingHorizontal: 20
   },
-  text: {
-    fontSize: 20,
-    fontWeight: '600',
-    color: 'red'
-  },
-  buttonGroup: {
-    marginTop: 10,
-    flexDirection: 'row',
-    gap: 4,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonRed: {
-    color: 'red'
-  }
+  title: { fontSize: 20, fontWeight: '600', color: 'red' },
+  view: { backgroundColor: 'pink', marginVertical: 10, padding: 20, borderRadius: 5 }
 });
