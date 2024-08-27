207d0a40739912d8dc8c940b13f7af757249b376
Ly Tran Hoang Hieu <lthhieu12.14lvl@gmail.com>
2024-08-27 22:43:29 +0700

    todo app basic

--- App.tsx
+++ App.tsx
@@ -1,48 +1,47 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList, TouchableOpacity } from 'react-native';
 
-interface IStudents {
-  id: number, name: string, age: number
+interface ITodo {
+  id: number,
+  name: string
 }
-
 export default function App() {
-  const [students, setStudents] = useState<IStudents[]>([
-    { id: 1, name: 'Hieu1', age: 18 },
-    { id: 2, name: 'Hieu2', age: 18 },
-    { id: 3, name: 'Hieu3', age: 18 },
-    { id: 4, name: 'Hieu4', age: 18 },
-    { id: 5, name: 'Hieu5', age: 18 },
-    { id: 6, name: 'Hieu6', age: 18 },
-    { id: 7, name: 'Hieu7', age: 18 },
-    { id: 8, name: 'Hieu8', age: 18 },
-    { id: 9, name: 'Hieu9', age: 18 },
-    { id: 10, name: 'Hieu10', age: 18 },
-    { id: 11, name: 'Hieu11', age: 18 },
-    { id: 12, name: 'Hieu12', age: 18 },
-    { id: 13, name: 'Hieu13', age: 18 },
-    { id: 14, name: 'Hieu14', age: 18 },
-    { id: 15, name: 'Hieu15', age: 18 },
-    { id: 16, name: 'Hieu16', age: 18 },
-  ])
-  const renderItem = ({ item }: { item: IStudents }) => {
-    return (<View style={styles.view}>
-      <Text>{item.name}</Text>
-    </View>)
+  const [todo, setTodo] = useState<string>('')
+  const [todos, setTodos] = useState<ITodo[]>([])
+  const addNewTodo = (newTodo: string) => {
+    if (newTodo === '') { alert('Please type todo'); return; }
+    setTodos(oldTodos => [...oldTodos,
+    { id: Math.floor(Math.random() * 100000), name: newTodo }
+    ]);
+    setTodo('')
   }
+  const removeTodo = (item: ITodo) => {
+    setTodos(todos.filter(el => { return el.id != item.id }))
+  }
+  const renderItem = ({ item }: { item: ITodo }) => {
+    return (
+      <Text onPress={() => {
+        removeTodo(item)
+      }} style={styles.view}>{item.name}</Text>
+    );
+  };
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Hello world</Text>
+      <View style={styles.body}>
+        <TextInput style={styles.input} placeholder='New todo'
+          onChangeText={setTodo}
+          value={todo} />
+        <TouchableOpacity style={styles.addNewTodoButton} onPress={() => { addNewTodo(todo) }}>
+          <Text style={styles.textAddNew}>Add todo</Text>
+        </TouchableOpacity>
+      </View>
       <FlatList
-        data={students}
+        style={styles.body2}
+        data={todos}
+        renderItem={renderItem}
         keyExtractor={item => item.id + ''}
-        renderItem={renderItem}></FlatList>
-      {/* <ScrollView>
-        {students.map((item) => {
-          return (<View style={styles.view} key={item.id}>
-            <Text>{item.name}</Text>
-          </View>)
-        })}
-      </ScrollView> */}
+      />
     </View>
   );
 }
@@ -51,11 +50,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    // alignItems: 'flex-start',
-    // justifyContent: 'flex-start',
     paddingTop: 40,
-    paddingHorizontal: 20
   },
-  title: { fontSize: 20, fontWeight: '600', color: 'red' },
+  title: {
+    fontSize: 20, fontWeight: '600', color: 'red',
+    backgroundColor: 'orange', paddingHorizontal: 20, textAlign: 'center', paddingVertical: 10
+  },
+  body: {
+    marginHorizontal: 50,
+  },
+  body2: {
+    marginHorizontal: 50,
+    marginTop: 10
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderColor: 'green',
+    marginVertical: 20,
+    paddingHorizontal: 10
+  },
+  addNewTodoButton: {
+    alignItems: 'center',
+    backgroundColor: '#4582e4',
+    padding: 10,
+    borderRadius: 5,
+  },
+  textAddNew: { textTransform: 'uppercase', fontWeight: '600', color: '#fff' },
   view: { backgroundColor: 'pink', marginVertical: 10, padding: 20, borderRadius: 5 }
 });
