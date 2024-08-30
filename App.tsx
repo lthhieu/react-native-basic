ab21beeb87254d480233543ed82df220db589c0d
Ly Tran Hoang Hieu <lthhieu12.14lvl@gmail.com>
2024-08-30 21:57:51 +0700

    using icon

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList, Pressable, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
-import FlexBox from './components/flex.box';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 interface ITodo {
   id: number,
@@ -26,35 +26,36 @@ export default function App() {
   }
   const renderItem = ({ item }: { item: ITodo }) => {
     return (
-      <Pressable style={(({ pressed }) => ({ opacity: pressed ? 0.5 : 1 }))} onPress={() => {
-        removeTodo(item.id)
-      }}>
-        <Text style={styles.view}>{item.name}</Text>
+      <Pressable style={(({ pressed }) => ({ opacity: pressed ? 0.5 : 1 }))} >
+        <View style={styles.listItem}>
+          <Text>{item.name}</Text>
+          <AntDesign onPress={() => removeTodo(item.id)} name="close" size={24} color="black" />
+        </View>
       </Pressable>
 
     );
   };
   return (
-    // <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-    //   <View style={styles.container}>
-    //     <Text style={styles.title}>Hello world</Text>
-    //     <View style={styles.body}>
-    //       <TextInput style={styles.input} placeholder='Tên công việc mới'
-    //         onChangeText={setTodo}
-    //         value={todo} />
-    //       <Pressable style={styles.addNewTodoButton} onPress={() => { addNewTodo(todo) }}>
-    //         <Text style={styles.textAddNew}>Thêm công việc mới</Text>
-    //       </Pressable>
-    //     </View>
-    //     <FlatList
-    //       style={styles.body2}
-    //       data={todos}
-    //       renderItem={renderItem}
-    //       keyExtractor={item => item.id + ''}
-    //     />
-    //   </View>
-    // </TouchableWithoutFeedback>
-    <FlexBox />
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello world</Text>
+        <View style={styles.body}>
+          <TextInput style={styles.input} placeholder='Tên công việc mới'
+            onChangeText={setTodo}
+            value={todo} />
+          <Pressable style={styles.addNewTodoButton} onPress={() => { addNewTodo(todo) }}>
+            <Text style={styles.textAddNew}>Thêm công việc mới</Text>
+          </Pressable>
+        </View>
+        <View style={styles.body2}>
+          <FlatList
+            data={todos}
+            renderItem={renderItem}
+            keyExtractor={item => item.id + ''}
+          />
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -65,15 +66,15 @@ const styles = StyleSheet.create({
     paddingTop: 40,
   },
   title: {
-    fontSize: 20, fontWeight: '600', color: 'red',
-    backgroundColor: 'orange', paddingHorizontal: 20, textAlign: 'center', paddingVertical: 10
+    fontSize: 20, fontWeight: '600', color: '#F05A7E',
+    backgroundColor: '#FABC3F', paddingHorizontal: 20, textAlign: 'center', paddingVertical: 10
   },
   body: {
     marginHorizontal: 50,
   },
   body2: {
     marginHorizontal: 50,
-    marginTop: 10
+    marginTop: 10, flex: 1
   },
   input: {
     borderBottomWidth: 1,
@@ -88,5 +89,5 @@ const styles = StyleSheet.create({
     borderRadius: 5,
   },
   textAddNew: { textTransform: 'uppercase', fontWeight: '600', color: '#fff' },
-  view: { backgroundColor: 'pink', marginVertical: 10, padding: 20, borderRadius: 5 }
+  listItem: { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', gap: 6, backgroundColor: 'pink', marginVertical: 10, padding: 20, borderRadius: 5 }
 });
