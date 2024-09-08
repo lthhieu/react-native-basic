15b650545b9519109667a32c66b721d9f4cd41ff
Ly Tran Hoang Hieu <lthhieu12.14lvl@gmail.com>
2024-09-08 10:11:20 +0700

    custom header 2

--- components/navigation/app.drawer.tsx
+++ components/navigation/app.drawer.tsx
@@ -3,19 +3,21 @@ import AboutScreen from "../review/about";
 import AppStack from "./app.stack";
 import { RootDrawerParamList } from "../../utils/navigation";
 import { PACIFICO } from "../../utils/constant";
-import { Text, View } from "react-native";
 
 const AppDrawer = () => {
     const Drawer = createDrawerNavigator<RootDrawerParamList>();
     return (
         <Drawer.Navigator>
-            <Drawer.Screen name="HomeLayout" component={AppStack} options={{ title: 'Trang chủ', headerShown: false }} />
-            <Drawer.Screen name="About" component={AboutScreen} options={headerCss} />
+            <Drawer.Screen name="HomeLayout" component={AppStack} options={{ ...headerBase, drawerLabel: 'Trang chủ', headerShown: false }} />
+            <Drawer.Screen name="About" component={AboutScreen} options={{ ...headerBase, drawerLabel: 'Thông tin', title: "Thông tin" }} />
         </Drawer.Navigator>
     )
 }
-export const headerCss: DrawerNavigationOptions = {
-    title: "Thông tin",
+
+export const headerBase: DrawerNavigationOptions = {
+    drawerLabelStyle: { fontFamily: PACIFICO },
+    drawerActiveBackgroundColor: '#FF8A8A',
+    drawerActiveTintColor: '#fff',
     headerStyle: {
         backgroundColor: '#FF8A8A',
     },
